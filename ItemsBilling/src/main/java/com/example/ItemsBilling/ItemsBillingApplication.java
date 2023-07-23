52b56c7bcbdcdb4defa0bd2250437d4b083274b9
Ram Singhal <ram.s@aptcoder.com>
2023-07-23 19:52:19 +0530

    Initial commit

---
+++ ItemsBilling/src/main/java/com/example/ItemsBilling/ItemsBillingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ItemsBilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsBillingApplication.class, args);
+	}
+
+}
